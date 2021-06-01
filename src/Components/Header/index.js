21a4cbdb58fe0cad3f141350bc42f2b36d2c9579
Haddawi <muhamadhaddawi@gmail.com>
2021-06-02 01:00:58 +0700

    feature-header: added header component with style

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,18 +1,39 @@
 import { Grid } from '@material-ui/core';
-
-const HeaderStyles = {
-  border: "1px solid purple"
-};
+import Logo from '../Assets/logo-damogo.png';
+import './index.css';
 
 const Header = () => {
   return (
     <Grid
       container
-      direction="column"
-      alignItems="center"
-      style={HeaderStyles}
+      direction="row"
+      alignItems="space-between"
+      className="header-root"
     >
-      <h1>Header</h1>
+      <Grid item xs={6}>
+        <img className="logo-damogo" src={Logo} alt="DamoGo"></img>
+      </Grid>
+      <Grid 
+        item 
+        xs={6}
+        container
+        direction="row"
+        justify="flex-end"
+        alignItems="center"
+      >
+        <Grid className="top-anchor">
+          <a className="active" href="">Beranda</a>
+          <div className="dot active" />
+        </Grid>
+        <Grid className="top-anchor">
+          <a href="">Restoran</a>
+          <div className="dot" />
+        </Grid>
+        <Grid className="top-anchor">
+          <a href="">Supplier</a>
+          <div className="dot" />
+        </Grid>
+      </Grid>
     </Grid>
   )
 };
