9a12b6ad628ecbdef7a46b2741bac21ab44e1b60
Haddawi <muhamadhaddawi@gmail.com>
2021-06-03 21:50:47 +0700

    fix: padding each container & move global styling

--- src/App.js
+++ src/App.js
@@ -4,21 +4,16 @@ import Contents from './Components/Contents';
 import Footer from './Components/Footer';
 import './App.css';
 
-const AppStyles = {
-  padding: "0 120px"
-};
-
 const App = () => {
   return (
     <div className="root">
       <Grid
         container
-        style={ AppStyles }
       >
         <Header />
         <Contents />
+        <Footer />
       </Grid>
-      <Footer />
     </div>
   )
 };
